fad01fb724490649899443e9b4d99c63cddb3c31
italo cruz <italocruz@gmail.com>
2025-03-18 16:52:10 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+let listaCarros = [
+    {
+        "nome": "Czinger 21c",
+        "img": "images/czinger-21c.webp",
+        "descricao": ` O Czinger 21C é um hipercarro híbrido biturbo projetado pela
+                  Czinger Vehicles. O carro foi inspirado na aeronave Lockheed
+                  SR-71 “Blackbird”, utilizada pelos americanos durante a Guerra
+                  Fria.`
+    },
+    {
+        "nome": "De Tomaso P72",
+        "img": "images/de-tomaso.webp",
+        "descricao": `O De Tomaso P72 é um superdesportivo italiano que foi revelado
+                  em 2019. A marca italiana anunciou que serão fabricadas apenas
+                  72 unidades do modelo.` 
+    },
+    {
+        "nome": "Drako GTE",
+        "img": "images/drako-gte.webp",
+        "descricao": `O Drako GTE é um supercarro elétrico norte-americano que tem
+                  quatro motores elétricos, um em cada roda. O carro é baseado
+                  no Fisker Karma e tem uma produção limitada a 25 unidades.` 
+    },
+    {
+        "nome": "Ferrari La Ferrari",
+        "img": "images/ferrari-laferrari.webpg",
+        "descricao": `A Ferrari LaFerrari é um superesportivo híbrido, com motor V12
+                  a gasolina e motor elétrico. Foi o primeiro carro híbrido de
+                  produção da Ferrari.` 
+    },
+    {
+        "nome": "Mclaren Elva",
+        "img": "images/mclaren-elva.webpg",
+        "descricao": `O McLaren Elva é um carro esportivo conversível, com motor
+                  central V8 biturbo, carroceria em fibra de carbono e design
+                  aerodinâmico.` 
+    },
+    {
+        "nome": "Pagani Huayra",
+        "img": "images/pagani-huayra.webpg",
+        "descricao": `O Pagani Huayra é um superesportivo com motor V12 central, que
+                  foi lançado em 2011. O nome Huayra vem da deusa do vento.` 
+    },
+
+    
+]
